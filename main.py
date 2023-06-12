496ca8f847dc78b43fe2b965b1f149be922591ba
Even Lauvsnes <even.lauvsnes99@gmail.com>
2023-06-12 15:44:40 -0500

    added reduced-row-echolon calculation

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
+from typing import List
 from fastapi import FastAPI, HTTPException
+from fastapi.responses import JSONResponse
 from fastapi.middleware.cors import CORSMiddleware
-from sympy import symbols, simplify, sympify
+from sympy import symbols, simplify, sympify, Matrix
 from pydantic import BaseModel
 from urllib.parse import unquote
+import json
 
 app = FastAPI()
 
@@ -17,12 +20,29 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-
 x = symbols("x")
 
 
 class Expression(BaseModel):
     expression: str
+
+
+class MatrixModel(BaseModel):
+    matrix: List[List[float]]
+
+
+@app.post("/rref/")
+async def get_rref(matrix: MatrixModel):
+    try:
+        sympy_matrix = Matrix(matrix.matrix)
+        rref_matrix = sympy_matrix.rref()[0]
+        return {"rref_matrix": str(rref_matrix)}
+    except ValueError as e:
+        print(f"ValueError: {e}")
+        raise HTTPException(status_code=400, detail=str(e))
+    except Exception as e:
+        print(f"General error: {e}")
+        raise HTTPException(status_code=500, detail=str(e))
 
 
 @app.post("/simplify/")
