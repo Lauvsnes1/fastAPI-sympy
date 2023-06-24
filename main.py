74da0ef136780f2da4337d6ee9ce16e2a7cf81b2
Even Lauvsnes <even.lauvsnes99@gmail.com>
2023-06-23 19:33:48 -0500

    failed trying to deploy to aws

--- main.py
+++ main.py
@@ -6,19 +6,21 @@ from sympy import symbols, simplify, sympify, Matrix
 from pydantic import BaseModel
 from urllib.parse import unquote
 import json
+from mangum import Mangum
 
 app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:3000",
-        "https://lauvsnes1.github.io",
-    ],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+
+handler = Mangum(app, api_gateway_base_path=None)
+
 
 x = symbols("x")
 
@@ -33,16 +35,21 @@ class MatrixModel(BaseModel):
 
 @app.post("/rref/")
 async def get_rref(matrix: MatrixModel):
+    print("matrix:", matrix)
     try:
         sympy_matrix = Matrix(matrix.matrix)
         rref_matrix = sympy_matrix.rref()[0]
-        return {"rref_matrix": str(rref_matrix)}
+        return JSONResponse(
+            content={"rref_matrix": str(rref_matrix)}, media_type="application/json"
+        )
     except ValueError as e:
         print(f"ValueError: {e}")
-        raise HTTPException(status_code=400, detail=str(e))
+        raise HTTPException(
+            status_code=400, detail="Invalid data. Please check your input."
+        )
     except Exception as e:
         print(f"General error: {e}")
-        raise HTTPException(status_code=500, detail=str(e))
+        raise HTTPException(status_code=500, detail="An unexpected error occurred.")
 
 
 @app.post("/simplify/")
@@ -56,7 +63,9 @@ async def simplify_expression(data: Expression):
         simplified_expression = simplify(sympy_expression)
 
         # Convert the result to a string and return it
-        return {"simplified_expression": str(simplified_expression)}
-
+        return JSONResponse(
+            content={"simplified_expression": str(simplified_expression)},
+            media_type="application/json",
+        )
     except Exception as e:
         raise HTTPException(status_code=400, detail=str(e))
