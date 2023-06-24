2a13d3bb34f536bb1b1c6f1af54ce99f9370a06d
Even Lauvsnes <even.lauvsnes99@gmail.com>
2023-06-23 20:04:29 -0500

    adding origin path

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000/", "https://lauvsnes1.github.io/"],
+    allow_origins=["http://localhost:3000", "https://lauvsnes1.github.io"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
